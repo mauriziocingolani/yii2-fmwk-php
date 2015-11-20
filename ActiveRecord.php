b0ef6728b3ed605d3060dbf2ffb13e4cea24dbfe
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2015-11-20 14:16:39 +0100

    Modifiche a ActiveRecord e NamedActiveRecord

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -6,7 +6,7 @@ namespace mauriziocingolani\yii2fmwkphp;
  * Estende yii\db\ActiveRecord aggiungendo funzionalità e utilità.
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0
+ * @version 1.0.1
  */
 abstract class ActiveRecord extends \yii\db\ActiveRecord {
 
@@ -26,6 +26,28 @@ abstract class ActiveRecord extends \yii\db\ActiveRecord {
      */
     public static function ItalianToMysqlDate($italianDate) {
         return join('-', array_reverse(preg_split("/[\/]/", $italianDate)));
+    }
+
+    /**
+     * Converte la data dal formato 'Y-m-d H:i[:s]'  a 'd/m/Y H:i[:s]'.
+     * @param string $string Data e ora in formato MySQL
+     * @param boolean $showSeconds True per mostrare anche i secondi
+     * @return string Data e ora in formato italiano
+     */
+    public static function MysqlToItalianDateTime($string, $showSeconds = false) {
+        $split = preg_split('/[ ]/', $string);
+        return self::MysqlToItalianDate($split[0]) . ' ' . substr($split[1], 0, $showSeconds ? 8 : 5);
+    }
+
+    /**
+     * Converte la data dal formato 'd/m/Y H:i[:s])'  a 'Y-m-d H:i:s'. La presenza dei secondi
+     * dipende dalla presenza nella stringa da convertire ('00' in caso di assenza).
+     * @param string $string Data e ora in formato italiano
+     * @return string Data in formato MySQL
+     */
+    public static function ItalianToMysqlDateTime($string) {
+        $split = preg_split('/[ ]/', $string);
+        return self::ItalianToMysqlDate($split[0]) . ' ' . $split[1] . (count($split) == 3 ? ':' . $split[2] : ':00');
     }
 
 }
--- NamedActiveRecord.php
+++ NamedActiveRecord.php
@@ -10,24 +10,36 @@ use yii\db\ActiveRecord;
  * @property string $LastName Cognome della persona
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0
+ * @version 1.0.1
  */
 abstract class NamedActiveRecord extends ActiveRecord {
 
     /**
-     * Restituisce il nome completo della persona (nome e cognome).
+     * Restituisce il nome completo della persona (nome e/o cognome)
+     * oppure <code>null</code> se nessuno dei due è definito.
      * @return string Nome completo
      */
     public function getCompleteName() {
-        return $this->FirstName . ' ' . $this->LastName;
+        if ($this->FirstName || $this->LastName) :
+            return (strlen($this->FirstName) > 0 ? $this->FirstName : '') .
+                    (strlen($this->FirstName) > 0 && strlen($this->LastName) > 0 ? ' ' : '') .
+                    (strlen($this->LastName) > 0 ? $this->LastName : '');
+        endif;
+        return null;
     }
 
     /**
-     * Restituisce il nome completo della persona (cognome e nome).
+     * Restituisce il nome completo della persona (cognome e/o nome)
+     * oppure <code>null</code> se nessuno dei due è definito.
      * @return string Nome completo (con cognome prima del nome)
      */
     public function getCompleteNameReversed() {
-        return $this->LastName . ' ' . $this->FirstName;
+        if ($this->FirstName || $this->LastName) :
+            return (strlen($this->LastName) > 0 ? $this->LastName : '') .
+                    (strlen($this->FirstName) > 0 && strlen($this->LastName) > 0 ? ' ' : '') .
+                    (strlen($this->FirstName) > 0 ? $this->LastName : '');
+        endif;
+        return null;
     }
 
 }
