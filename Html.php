d069d828d3fedc131b856d13d9790f015670f155
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-03-14 13:46:31 +0100

    Aggiunto metodo faar a Html.php

--- Html.php
+++ Html.php
@@ -6,7 +6,7 @@ namespace mauriziocingolani\yii2fmwkphp;
  * Estende la classe yii\helpers\Html aggiungendo metodi e funzionalità.
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0
+ * @version 1.0.1
  */
 class Html extends \yii\helpers\Html {
 
@@ -22,6 +22,21 @@ class Html extends \yii\helpers\Html {
      */
     public static function faa($icon, $text = null, $url = null, array $options = array()) {
         $t = '<i class="fa fa-' . $icon . '"></i>' . ($text && strlen($text) > 0 ? ' ' . $text : '');
+        return self::a($t, $url, $options);
+    }
+
+    /**
+     * Genera un tag <a> con i parametri assegnati. E' un wrapper per il metodo {@link yii\helpers\Html::a}
+     * che aggiunge l'icona specificata dopo il testo.
+     * Presuppone che venga caricato il file css di FontAwesome.
+     * @param string $icon Icona FontAwesome
+     * @param string $text Testo del link
+     * @param string $url Url del link
+     * @param array $options Opzioni HTML
+     * @return string Tag generato
+     */
+    public static function faar($icon, $text, $url = null, array $options = array()) {
+        $t = $text . ' <i class="fa fa-' . $icon . '"></i>';
         return self::a($t, $url, $options);
     }
 
