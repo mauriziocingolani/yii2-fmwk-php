dfaed68f8f5caa86a36f05d82bf1f42404247f21
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-08-14 09:31:53 +0200

    Generalizzazione nome proprietà su NamedActiveRecord

--- NamedActiveRecord.php
+++ NamedActiveRecord.php
@@ -6,11 +6,14 @@ use yii\db\ActiveRecord;
 
 /**
  * Rappresenta una tabella di database con nome e cognome.
+ * Presuppone che il nome degli attributi che rappresentano nome
+ * e cognome siano 'FirstName' e 'LastName' rispettivamente.
+ * Per modificare il nome degli attributi va sovrascritto il metodo {@link GetFields}.
  * @property string $FirstName Nome della persona
  * @property string $LastName Cognome della persona
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0.1
+ * @version 1.0.2
  */
 abstract class NamedActiveRecord extends ActiveRecord {
 
@@ -20,10 +23,11 @@ abstract class NamedActiveRecord extends ActiveRecord {
      * @return string Nome completo
      */
     public function getCompleteName() {
-        if ($this->FirstName || $this->LastName) :
-            return (strlen($this->FirstName) > 0 ? $this->FirstName : '') .
-                    (strlen($this->FirstName) > 0 && strlen($this->LastName) > 0 ? ' ' : '') .
-                    (strlen($this->LastName) > 0 ? $this->LastName : '');
+        list($f, $l) = static::GetFields();
+        if ($this->$f || $this->$l) :
+            return (strlen($this->$f) > 0 ? $this->$f : '') .
+                    (strlen($this->$f) > 0 && strlen($this->$l) > 0 ? ' ' : '') .
+                    (strlen($this->$l) > 0 ? $this->$l : '');
         endif;
         return null;
     }
@@ -34,12 +38,26 @@ abstract class NamedActiveRecord extends ActiveRecord {
      * @return string Nome completo (con cognome prima del nome)
      */
     public function getCompleteNameReversed() {
-        if ($this->FirstName || $this->LastName) :
-            return (strlen($this->LastName) > 0 ? $this->LastName : '') .
-                    (strlen($this->FirstName) > 0 && strlen($this->LastName) > 0 ? ' ' : '') .
-                    (strlen($this->FirstName) > 0 ? $this->LastName : '');
+        list($f, $l) = static::GetFields();
+        if ($this->$f || $this->$l) :
+            return (strlen($this->$l) > 0 ? $this->$l : '') .
+                    (strlen($this->$f) > 0 && strlen($this->$l) > 0 ? ' ' : '') .
+                    (strlen($this->$f) > 0 ? $this->$l : '');
         endif;
         return null;
+    }
+
+    /**
+     * Restituisce un array con due elementi stringa:
+     * <ul>
+     * <li>Nome della proprietà che rappresenta il nome
+     * <li>Nome della proprietà che rappresenta il congome
+     * </ul>
+     * Può essere sovrascritto per modificare il nome di default delle due proprietà.
+     * @return array Nomi degii attributi nome e cognome
+     */
+    public static function GetFields() {
+        return ['FirstName', 'LastName'];
     }
 
 }
