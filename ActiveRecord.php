cae2a321a96de289c0eacf34b26162149a5a8332
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2018-07-25 14:14:01 +0200

    Baco mancanza id utente modificatore su ActiveRecord

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -22,7 +22,7 @@ use rmrevin\yii\fontawesome\FA;
  * 
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0.11
+ * @version 1.0.12
  */
 abstract class ActiveRecord extends \yii\db\ActiveRecord {
 
@@ -45,7 +45,7 @@ abstract class ActiveRecord extends \yii\db\ActiveRecord {
         if (isset($this->Modificato) && $this->Modificato)
             $s .= Html::tag('br') .
                     'Ultima modifica il ' . date('d-m-Y', strtotime($this->Modificato)) .
-                    " da parte di <strong>{$this->modificatore->UserName}</strong>";
+                    ($this->ModificatoDa ? " da parte di <strong>{$this->modificatore->UserName}</strong>" : null);
         return Html::tag('p', $s, ['class' => 'created']);
     }
 
