20ecd72df0b6f2c4f40fbd6e818969e0f419a048
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2019-09-26 07:48:07 +0200

    Metodo isControllerAction su Controller

--- Controller.php
+++ Controller.php
@@ -8,7 +8,7 @@ use Yii;
  * Estende la classe Controller aggiungendo alcune funzionalità.
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0.2
+ * @version 1.0.3
  */
 class Controller extends \yii\web\Controller {
 
@@ -44,6 +44,22 @@ class Controller extends \yii\web\Controller {
      */
     public function getIsDebug() {
         return isset(Yii::$app->modules['debug']) && in_array(Yii::$app->request->userIP, Yii::$app->modules['debug']->allowedIPs);
+    }
+
+    /**
+     * Verifica se il controller ed eventualmente l'azione indicata sono quelle attuali.
+     * @param array|string $controller Nome del controller (o array di nomi)
+     * @param type $action Nome dell'azione (o array di nomi)
+     * @return bool True se il controller e l'azione attuali sono tra quelli indicati
+     */
+    public function isControllerAction($controller, $action = null): bool {
+        if ((is_array($controller) && !in_array(Yii::$app->controller->id, $controller)) || (is_string($controller) && Yii::$app->controller->id != $controller))
+            return false;
+        if (!is_null($action)) :
+            if ((is_array($action) && !in_array(Yii::$app->controller->action->id, $action)) || (is_string($action) && Yii::$app->controller->action->id != $action))
+                return false;
+        endif;
+        return true;
     }
 
 }
