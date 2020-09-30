7f9e299d1039baa6272c43c9f43685e347ec55fa
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2020-09-30 15:10:04 +0200

    Baco validazione codice fiscale con lettere minuscole (2)

--- FiscalCodeValidator.php
+++ FiscalCodeValidator.php
@@ -8,7 +8,7 @@ use yii\validators\Validator;
  * Validatore per campi che contengono un codice fiscale.
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0.3
+ * @version 1.0.4
  * @link http://blog.marketto.it/2016/01/regex-validazione-codice-fiscale-con-omocodia/ Fonte regex di validazione
  */
 class FiscalCodeValidator extends Validator {
@@ -66,7 +66,7 @@ class FiscalCodeValidator extends Validator {
             for ($i = 0; $i < 15; $i++) :
                 $sum += ($i % 2 == 0 ? self::ODD[strtoupper($code[$i])] : self::EVEN[strtoupper($code[$i])]);
             endfor;
-            if ($code[15] != (self::REST[$sum % 26]))
+            if (strtoupper($code[15]) != (self::REST[$sum % 26]))
                 $this->addError($model, $attribute, $this->controlCharacterMessage);
         endif;
     }
